9e90a76cd71fba931dfe8c569e11e8bdd445f5f7
Rich Buggy <rich@buggy.id.au>
2017-03-04 17:23:56 +1100

    Basic React App using TypeScript and Webpack

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# shopify-react-starter
-Shopify React Starter Kit
+# Shopify React Starter Kit
+This starter kit is designed to get you up and running quickly when building a new [Shopify](https://www.shopify.com/?ref=growingecommerce) embedded app using React. While it assumes you have a backend based on [Shopify Serverless Kit](http://github.com/buggy/shopify-embedded-app) you can build your own backend using any language/framework you prefer.
+
+# Requirements
+Coming soon...
+
+# Installing
+Coming soon...
+
+# Copyright
+See the LICENCE file
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+
+module.exports = (env) => {
+    return {
+        entry: "./src/index.tsx",
+
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "dist"),
+            publicPath: 'dist',
+        },
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: env.prod ? "source-map" : "eval",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".tsx", ".ts", ".js"]
+        },
+
+        module: {
+            rules: [
+                { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+                { test: /\.tsx$/, loader: "source-map-loader", enforce: "pre" },
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader", exclude: /node_modules/ }
+            ]
+        },
+
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM"
+        },
+
+        devServer: {
+            port: 5000,
+            https: true,
+            historyApiFallback: true
+        }
+    }
+};

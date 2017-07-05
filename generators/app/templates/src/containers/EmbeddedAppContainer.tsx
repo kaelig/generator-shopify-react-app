8e47be6c0e6700d7ce3459b07e591cdc7e50ea9c
Rich Buggy <rich@buggy.id.au>
2017-07-05 21:22:21 +1000

    Upgrade Polaris 1.1.1 resolves #8

--- generators/app/templates/src/containers/EmbeddedAppContainer.tsx
+++ generators/app/templates/src/containers/EmbeddedAppContainer.tsx
@@ -2,25 +2,13 @@ import { EmbeddedApp } from "@shopify/polaris/embedded";
 import * as React from "react";
 import { config } from "../config";
 
-// tslint:disable-next-line:no-namespace
-declare namespace ShopifyApp {
-    function init(params: any): any;
-}
-
 // This component conditionally includes the EmbeddedApp component based on the enableEmbedded setting in the config
 export function EmbeddedAppContainer(props: { children?: any }): JSX.Element {
     if (config.enableEmbedded) {
         const shop = localStorage.getItem(config.shopKey);
 
-        // TODO - I'm 99% sure this shouldn't be required. It looks like the EmbeddedApp component doesn't cause the
-        // page to load inside the Shopify Admin the way this does. See https://github.com/Shopify/polaris/issues/28
-        ShopifyApp.init({
-            apiKey: config.shopifyApiKey,
-            shopOrigin: `https://${shop}`,
-        });
-
         return (
-            <EmbeddedApp apiKey={config.shopifyApiKey} shopOrigin={`https://${shop}`}>
+            <EmbeddedApp apiKey={config.shopifyApiKey} shopOrigin={`https://${shop}`} forceRedirect={true}>
                 {props.children}
             </EmbeddedApp>
         );
